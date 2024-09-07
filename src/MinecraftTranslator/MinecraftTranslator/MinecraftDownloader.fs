abffd6d426a5b69f388b0e9a0799bf642784f904
LAntoine <LAntoine@users.noreply.github.com>
2024-09-07 15:20:32 +0000

    Make program a command line application

--- src/MinecraftTranslator/MinecraftTranslator/MinecraftDownloader.fs
+++ src/MinecraftTranslator/MinecraftTranslator/MinecraftDownloader.fs
@@ -6,22 +6,30 @@ open System.Text.Json
 open CmlLib.Core
 open MinecraftTranslator.Domain
 
-let latestVersion =
+let getVersions () = 
     MinecraftLauncher().GetAllVersionsAsync().AsTask()
     |> Async.AwaitTask
     |> Async.RunSynchronously
     |> Seq.filter (fun version -> version.Type = "release")
+
+let getVersionsNames () = 
+    getVersions()
+    |> Seq.sortBy (fun version -> version.ReleaseTime)  
+    |> Seq.map (fun version -> version.Name)  
+
+let getLatestVersion () =
+    getVersions()
     |> Seq.maxBy (fun version -> version.ReleaseTime)
     |> (fun version -> version.Name)
 
 /// Download Minecraft and return the path where it has been saved.
-let download =
+let download () =
     System.Net.ServicePointManager.DefaultConnectionLimit <- 256
     let path = MinecraftPath(Path.Combine(Directory.GetCurrentDirectory(), "Minecraft"))
     printfn $"MinecraftPath: {path}"
     let launcher = MinecraftLauncher(path)
     launcher.ByteProgressChanged.Add(fun args -> printfn "%d bytes / %d bytes" args.ProgressedBytes args.TotalBytes)
-    launcher.InstallAsync(latestVersion).AsTask()
+    launcher.InstallAsync(getLatestVersion()).AsTask()
         |> Async.AwaitTask 
         |> Async.RunSynchronously
     path
@@ -33,7 +41,7 @@ type Index = { Objects: Map<string,MinecraftObject> }
 
 
 let getTranslation fileName =
-    let path = download
+    let path = download()
     let files = Directory.GetFiles(Path.Combine(path.Assets, "indexes"))
     let filePath = Seq.maxBy (fun file -> file) files
     let json = File.ReadAllText filePath
--- src/MinecraftTranslator/MinecraftTranslator/Program.fs
+++ src/MinecraftTranslator/MinecraftTranslator/Program.fs
@@ -1,6 +1,32 @@
+open CommandLine
+
+[<Verb("translate", HelpText = "Translate Minecraft to a given language.")>]
+type TranslateOptions = {
+  [<Option(Default = "zh_ln", HelpText = "Target language (zh_ln or zh_py).")>] language : string;
+  [<Option(Default = "latest", HelpText = "Minecraft version.")>] version : string;
+}
+
+[<Verb("list-versions", HelpText = "List Minecraft versions.")>]
+type ListVersionsOptions = {
+  [<Option(longName = "latest-only", HelpText = "Show only the latest version.")>] latestOnly : bool;
+}
+
+let runTranslateAndReturnExitCode opts =
+    let translation = MinecraftDownloader.getTranslation "minecraft/lang/zh_cn.json"
+    0
+
+let runListVersionstAndReturnExitCode (opts: ListVersionsOptions) = 
+    match opts.latestOnly with
+        | true -> printfn "%s" (MinecraftDownloader.getLatestVersion())
+        | false -> Seq.iter (printfn "%s") (MinecraftDownloader.getVersionsNames())
+    0
 
 [<EntryPoint>]
 let main args =
-    let translation = MinecraftDownloader.getTranslation "minecraft/lang/zh_cn.json"
-    
-    0
+  let result = Parser.Default.ParseArguments<TranslateOptions, ListVersionsOptions> args
+  match result with
+  | :? CommandLine.Parsed<obj> as command ->
+    match command.Value with
+    | :? TranslateOptions as opts -> runTranslateAndReturnExitCode opts
+    | :? ListVersionsOptions as opts -> runListVersionstAndReturnExitCode opts
+  | :? CommandLine.NotParsed<obj> -> 1
